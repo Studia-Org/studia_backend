49918b4e3d8f754cf5fced2bd709caafe284d66d
rogeranyor <rogeranyor@gmail.com>
2024-02-26 17:34:27 +0100

    Update lifecycles.js

--- src/api/qualification/content-types/qualification/lifecycles.js
+++ src/api/qualification/content-types/qualification/lifecycles.js
@@ -1,21 +1,35 @@
 module.exports = {
     async beforeCreate(event) {
-        const { group, activity, file, } = event.params.data;
+        const { group, activity, file, user } = event.params.data;
         //check if qualification exists for the group
         //when evaluator uploads a qualification we dont need to check if the activity has passed
         if (activity === undefined) {
             return
         }
+        if (group !== undefined) {
+            const qualification = await strapi.db.query('api::qualification.qualification').findOne({
+                where: {
+                    group: group,
+                    activity: activity
+                }
+            });
 
-        const qualification = await strapi.db.query('api::qualification.qualification').findOne({
-            where: {
-                group: group,
-                activity: activity
+            if (qualification) {
+                throw new Error("Qualification already exists for the group and activity");;
             }
-        });
+        }
+        console.log("user", user)
+        if (user !== undefined) {
+            const userQualification = await strapi.db.query('api::qualification.qualification').findOne({
+                where: {
+                    user: user,
+                    activity: activity
+                }
+            });
 
-        if (qualification) {
-            throw new Error("Qualification already exists for the group and activity");;
+            if (userQualification) {
+                throw new Error("Qualification already exists for the user and activity");;
+            }
         }
         const activityData = await strapi.db.query('api::activity.activity').findOne({
             where: {
@@ -30,12 +44,25 @@ module.exports = {
 
     },
     async beforeUpdate(event) {
-        const { group, activity, file } = event.params.data;
+        const { group, activity, file, updatedBy } = event.params.data;
         //when evaluator uploads a qualification we dont need to check if the activity has passed
+
 
         if (activity === undefined) {
             return
         }
+        if (updatedBy !== undefined) {
+            // check who is updating the qualification
+            const author = (
+                await strapi.entityService.findMany("admin::user", {
+                    filters: { id: updatedBy }
+                })
+            )[0];
+            if (author) return;
+
+        }
+
+
         const activityData = await strapi.db.query('api::activity.activity').findOne({
             where: {
                 id: activity
