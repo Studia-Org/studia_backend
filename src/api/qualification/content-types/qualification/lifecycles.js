cb6eaf91340f9f68d9ca9ce9ee87c6c67b4f6f2b
rogeranyor <rogeranyor@gmail.com>
2024-02-26 16:32:55 +0100

    check if its evaluator who wants to create or update

--- src/api/qualification/content-types/qualification/lifecycles.js
+++ src/api/qualification/content-types/qualification/lifecycles.js
@@ -1,7 +1,12 @@
 module.exports = {
     async beforeCreate(event) {
-        const { group, activity, file } = event.params.data;
+        const { group, activity, file, } = event.params.data;
         //check if qualification exists for the group
+        //when evaluator uploads a qualification we dont need to check if the activity has passed
+        if (activity === undefined) {
+            return
+        }
+
         const qualification = await strapi.db.query('api::qualification.qualification').findOne({
             where: {
                 group: group,
@@ -26,6 +31,11 @@ module.exports = {
     },
     async beforeUpdate(event) {
         const { group, activity, file } = event.params.data;
+        //when evaluator uploads a qualification we dont need to check if the activity has passed
+
+        if (activity === undefined) {
+            return
+        }
         const activityData = await strapi.db.query('api::activity.activity').findOne({
             where: {
                 id: activity
