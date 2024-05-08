659bd5ed0658bedc3efb74ea1c13e22e4e47ff6f
rogeranyor <rogeranyor@gmail.com>
2024-05-08 17:25:54 +0200

    now you can update groups

--- src/api/group/controllers/group.js
+++ src/api/group/controllers/group.js
@@ -18,23 +18,44 @@ module.exports = createCoreController('api::group.group',
                 return ctx.badRequest({ result: 'Activity id and groups are required' });
             }
             try {
-
-                await deleteGroups({ strapi, activityId });
-
-                groups.forEach(async (group) => {
-                    const create_group = await strapi.db.query("api::group.group").create({
-                        data: {
-                            users: group.map(user => user.id),
-                            publishedAt: new Date(),
-                            activity: activityId
-                        },
+                const firstGroup = [...groups[0]]
+                const lastElement = firstGroup.pop() // check if groups are already created and just need to be updated
+                const groupId = lastElement.groupId;
+                if (groupId) {
+                    groups.forEach(async (group) => {
+                        const lastElement = group.pop();
+                        const id = lastElement.groupId;
+                        const update_group = await strapi.db.query("api::group.group").update({
+                            where: {
+                                id: id
+                            },
+                            data: {
+                                users: group.map(user => user.id),
+                                publishedAt: new Date(),
+                                activity: activityId
+                            },
+                        });
                     });
-                });
+                }
+                else {
+                    await deleteGroups({ strapi, activityId });
+                    groups.forEach(async (group) => {
+                        const create_group = await strapi.db.query("api::group.group").create({
+                            data: {
+                                users: group.map(user => user.id),
+                                publishedAt: new Date(),
+                                activity: activityId
+                            },
+                        });
+                    });
+                }
+
                 ctx.response.status = 200;
                 return ctx
             }
             catch (err) {
-                await deleteGroups({ strapi, activityId });
+                //await deleteGroups({ strapi, activityId, newGroups: groups });
+                ctx.response.status = 500;
                 return ctx.internalServerError({ result: 'Error creating groups' });
             }
 
